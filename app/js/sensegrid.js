d4b9f32ba6c6a668278bf37f3a4bbf804c01208e
davidvina <davidvina@gmail.com>
2017-10-18 17:31:27 +0200

    update

--- app/js/sensegrid.js
+++ app/js/sensegrid.js
@@ -1,10 +1,9 @@
 var senseGridData = window.getComputedStyle(
-    document.querySelector('html'), ':after')    // selector
-    .getPropertyValue('content')                 // nombre propiedad Css
-    .replace(/^(")|(\\)|(")$/g, '');
+    document.querySelector('html'), ':after') // selector
+  .getPropertyValue('content') // nombre propiedad Css
+  .replace(/^(")|(\\)|(")$/g, '');
 
 //senseGridData = senseGridData.replace(/\\/g, '');
-
 
 console.log(senseGridData);
 
@@ -14,31 +13,30 @@ senseGridData = JSON.parse(senseGridData);
 // loop trought senseGridData object
 for (property in senseGridData) {
   if (senseGridData.hasOwnProperty(property)) {
-      console.log(property + " -> " + senseGridData[property]);
+    console.log(property + ' -> ' + senseGridData[property]);
   }
-} 
+}
 
 // create elements
 function createGrid() {
-    var containerGrid = document.createElement("DIV");
-    // add atributes
-    containerGrid.setAttribute("id", "grid_guide");
-    containerGrid.setAttribute("class", "btn_class");
-    
-    for (var i = 0; i < senseGridData.grid; i++) {
-        
-        // append elements to contaner 
-        var column = document.createElement("DIV");
-        column.setAttribute("id", "col_"+i+1);
-        column.setAttribute("class", "col");
-        column.appendChild(document.createTextNode(i+1));
+  var containerGrid = document.createElement('DIV');
+  // add atributes
+  containerGrid.setAttribute('id', 'grid_guide');
+  containerGrid.setAttribute('class', 'btn_class');
 
-        containerGrid.appendChild(column);  
+  for (var i = 0; i < senseGridData.grid; i++) {
 
-    }
+    // append elements to contaner
+    var column = document.createElement('DIV');
+    column.setAttribute('id', 'col_' + i + 1);
+    column.setAttribute('class', 'col');
+    column.appendChild(document.createTextNode(i + 1));
 
-    document.body.appendChild(containerGrid);
+    containerGrid.appendChild(column);
+
+  }
+
+  document.body.appendChild(containerGrid);
 }
 
-
-//createGrid();
+createGrid();
